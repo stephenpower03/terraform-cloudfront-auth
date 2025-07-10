9c11b7c0a4768fcca6962d7a735aa32c62a774e4
stephenpower03 <stephene.power@aerlingus.com>
2025-07-10 21:01:15 +0100

    remove unused S3-related output after switching to pre-provisioned bucket

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "s3_bucket" {
-  description = "The name of the S3 Bucket"
-  value       = aws_s3_bucket.default.id
-}
-
 output "cloudfront_arn" {
   description = "ARN of the Cloudfront Distribution"
   value       = aws_cloudfront_distribution.default.arn
