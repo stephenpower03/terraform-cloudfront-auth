323f2959dd563157e26b679635c2595462d9041d
Stephen Power <stephene.power@aerlingus.com>
2025-09-19 21:42:36 +0100

    fix: update provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 6.13.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
 provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
 }
-
